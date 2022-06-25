5ab63031cc165e2a3b6b95cdeeff635102007559
nickname838 <german.galitskov@gmail.com>
2022-06-25 12:38:00 +0700

    add tree node

--- encode.cpp
+++ encode.cpp
@@ -3,9 +3,28 @@
 
 using namespace std;
 
-// кодирование
-void encode (string name) {
+// структура узла дерева
+struct Node {
+    char ch;
+    int freq;
+    Node *left;
+    Node *right;
+};
 
+// создание узла
+Node *getnode(char ch, int freq, Node *left = nullptr, Node *right = nullptr) {
+    Node *node = new Node;
+    node->ch = ch;
+    node->freq = freq;
+    node->left = left;
+    node->right = right;
+    return node;
+}
+
+// кодирование
+void encode (string file) {
+    ifstream in(file, ios::binary);
+    if(!in) {puts("Input file doesn't exist"); return;}
 }
 
 int main() {
