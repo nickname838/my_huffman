e6a45c551196d8d52a9d0a8e2ca73415f683b353
nickname838 <german.galitskov@gmail.com>
2022-06-25 12:44:48 +0700

    add queue and tree creation

--- encode.cpp
+++ encode.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <fstream>
+#include <map>
+#include <queue>
 
 using namespace std;
 
@@ -21,10 +23,38 @@ Node *getnode(char ch, int freq, Node *left = nullptr, Node *right = nullptr) {
     return node;
 }
 
+// сравнение для очереди
+struct comp {
+    bool operator()(Node *l, Node *r) {return l->freq > r->freq;}
+};
+
+// создание дерева
+Node *maketree(priority_queue<Node *, vector<Node *>, comp> &pq) {
+    while(pq.size() > 1) {
+        Node *left = pq.top(); pq.pop();
+        Node *right = pq.top(); pq.pop();
+        uint64_t sum = left->freq + right->freq;
+        pq.push(getnode('\0', sum, left, right));
+    }
+    return pq.top();
+}
+
 // кодирование
 void encode (string file) {
     ifstream in(file, ios::binary);
     if(!in) {puts("Input file doesn't exist"); return;}
+
+    // частота байтов
+    char x; map<char, int> freq;
+    while(in.get(x)) freq[x]++;
+    in.clear(); in.seekg(0);
+
+    // очередь узлов
+    priority_queue<Node *, vector<Node *>, comp> pq;
+    for(auto pair: freq) pq.push(getnode(pair.first, pair.second));
+
+    // создание дерева
+    Node *root = maketree(pq);
 }
 
 int main() {
