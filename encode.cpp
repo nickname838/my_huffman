f5cd475e6acd87b8742c5df8beefbcbf7593814a
nickname838 <german.galitskov@gmail.com>
2022-06-25 12:25:38 +0700

    create encode.cpp

---
+++ encode.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+// кодирование
+void encode (string name) {
+
+}
+
+int main() {
+    string file;
+    cout << "Enter file's name: ";
+    cin >> file;
+}
